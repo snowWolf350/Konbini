ddd07fabc6c5ca22128935be8ab4142cd26e6159
snowwolf350 <pranet.v08@gmail.com>
2026-09-22 15:45:01 +0530

    Player rotates

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     float _playerSpeed = 5;
-
+    float _playerRotateSpeed = 5;
+    Vector3 _rotateDir;
     CharacterController _characterController;
 
     void Start()
@@ -21,8 +22,17 @@ public class Player : MonoBehaviour
         Vector2 inputVector = PlayerInput.Instance.GetInputVector();
         Vector3 moveDir = new Vector3(inputVector.y, 0, -inputVector.x);
 
+        _rotateDir = moveDir;
+
         Debug.Log(moveDir);
 
         _characterController.Move(moveDir * _playerSpeed * Time.deltaTime);
+        if (_rotateDir != Vector3.zero)
+        {
+            transform.forward = Vector3.Slerp(
+                transform.forward,
+                _rotateDir,
+                Time.deltaTime * _playerRotateSpeed);
+        }
     }
 }
